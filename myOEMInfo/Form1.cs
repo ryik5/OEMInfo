34c6f265cb734ddd1013c352761c794df91b8102
ryik5 <ryik.yuri@gmail.com>
2018-11-07 15:04:41 +0200

    Добавьте файлы проекта.

---
+++ myOEMInfo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myOEMInfo
+{
+    public partial class Form1 :Form
+    {
+        private Bitmap bmp;
+        private Bitmap Logo;
+        private string SupportURL;
+        private string SupportPhone;
+        private string Model;
+        private string Manufacturer;
+
+        private string sBaseRegisterLocal = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMinformation";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Properties.Resources.LogoRYIK);
+        }
+        private void SetRegistrySavedData()
+        {
+            using (Microsoft.Win32.RegistryKey EvUserKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(sBaseRegisterLocal))
+            {
+                EvUserKey.SetValue("SupportURL", SupportURL, Microsoft.Win32.RegistryValueKind.String);
+                EvUserKey.SetValue("SupportPhone", SupportPhone, Microsoft.Win32.RegistryValueKind.String);
+                EvUserKey.SetValue("Model", Model, Microsoft.Win32.RegistryValueKind.String);
+                EvUserKey.SetValue("Manufacturer", Manufacturer, Microsoft.Win32.RegistryValueKind.String);
+                EvUserKey.SetValue("Logo", Logo, Microsoft.Win32.RegistryValueKind.String);
+            }
+        }
+
+        private void CheckRegistrySavedData()
+        {
+            try
+            {
+                using (Microsoft.Win32.RegistryKey EvUserKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(sBaseRegisterLocal, Microsoft.Win32.RegistryKeyPermissionCheck.ReadSubTree, System.Security.AccessControl.RegistryRights.ReadKey))
+                {
+                    SupportURL = EvUserKey.GetValue("SupportURL").ToString();
+                    SupportPhone = EvUserKey.GetValue("SupportPhone").ToString();
+                    Model = EvUserKey.GetValue("Model").ToString();
+                    Manufacturer = EvUserKey.GetValue("Manufacturer").ToString();
+                    Logo = EvUserKey.GetValue("Logo").ToString();
+                }
+            } catch { }
+        }
+
+    }
+}
---
+++ myOEMInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("OEMInfo")]
+[assembly: AssemblyDescription("Put OEM Info")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RYIK")]
+[assembly: AssemblyProduct("OEMInfo")]
+[assembly: AssemblyCopyright("Copyright ©RYIK  2017")]
+[assembly: AssemblyTrademark("©RYIK")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5ca322f8-658f-4dee-9b9d-d9ca4675725a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
